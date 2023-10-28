3cbaa861d76452a07b782cf721c2f17bbc956dbe
iamenochchirima <iamenochchirima@gmail.com>
2023-10-28 18:24:32 +0200

    Core: canisters setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
-const frontendDirectory = "nexusblogs_frontend";
+const frontendDirectory = "nexusblocks_frontend";
 
 const frontend_entry = path.join("src", frontendDirectory, "src", "index.html");
 
